9170b5c12055c3a675096d65c873ff3dc9109214
thomd112 <114462199+thomd112@users.noreply.github.com>
2025-01-29 13:56:00 -0500

    adding float css transitions

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
-// Function to change the word displayed in the wordGrid
+
 function changeWord(word) {
     const wordGrid = document.getElementById("wordGrid");
-    wordGrid.innerHTML = ""; // Clear previous word
+    wordGrid.innerHTML = ""; 
 
-    // Create letter tiles for each letter of the word and add them to wordGrid
+   
     for (let letter of word) {
         let block = document.createElement("div");
         block.textContent = letter;
         wordGrid.appendChild(block);
     }
 
-    // Apply style changes to the button clicked
-    const clickedButton = event.target; // Get the button that was clicked
     
-    // Change the button's background color, text transformation, and other styles
-    clickedButton.style.backgroundColor = "green"; // Change background color
+    const clickedButton = event.target; 
+    clickedButton.style.backgroundColor = "#cae6ce"; 
+    clickedButton.style.transform = "rotate(360deg)"; 
   
-
-    clickedButton.style.animationPlayState = "paused"; // Pause animation on click
 }
 
 
 function moveButtonRandomly(button) {
     const windowWidth = window.innerWidth;
     const windowHeight = window.innerHeight;
+
 
  
     const randomX = Math.floor(Math.random() * (windowWidth - button.offsetWidth));
@@ -36,9 +34,8 @@ function moveButtonRandomly(button) {
 }
 
 
-const buttons = document.querySelectorAll('.button-box button');
+const buttons = document.querySelectorAll('.primary-box button');
 buttons.forEach(button => {
     setInterval(() => moveButtonRandomly(button), 2000);  
 });
-
 
